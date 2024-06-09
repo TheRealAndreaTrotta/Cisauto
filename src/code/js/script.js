eac625e15593d7870683e869caa50dca3430d3ed
TheRealAndreaTrotta <andreatrotta004@gmail.com>
2024-06-09 18:58:45 +0200

    PP

--- src/code/js/script.js
+++ src/code/js/script.js
@@ -32,9 +32,31 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-
+//ROTADE ICON
 function toggleRotation() {
     const icon = document.querySelector('.bx');
     icon.classList.toggle('rotated');
 }
+
+//CURTAIN EFFECT
+function toggleContent(element) {
+    const content = element.nextElementSibling;
+    const arrow = element.querySelector('.rotate');
+
+    if (content.classList.contains('active')) {
+        // Start closing transition
+        content.classList.remove('active');
+        arrow.classList.remove('open');
+    } else {
+        // Start opening transition
+        content.style.transition = 'max-height 0.5s ease'; // Set fast transition
+        content.classList.add('active');
+        arrow.classList.add('open');
+
+        setTimeout(() => {
+            content.style.transition = 'max-height 5s ease'; // Revert to slow transition
+        }, 500); // Duration of fast opening transition
+    }
+}
+
 
