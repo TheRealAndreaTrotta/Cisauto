a03682d7a0537098e42aeb2d6184665c7011e9e6
TheRealAndreaTrotta <andreatrotta004@gmail.com>
2024-06-19 15:33:03 +0200

    CONTACT

--- src/code/js/script.js
+++ src/code/js/script.js
@@ -27,18 +27,3 @@ function toggleRotation(iconId) {
 }
 
 // SEND BUTTON
-const button = document.querySelector("button");
-
-const clear =()=>{
-    setTimeout(() => {
-        button.classList.remove("effect2");
-    }, 1000);
-}
-
-button.addEventListener("click", () => {
-    button.classList.add("effect1");
-    setTimeout(() => {
-        button.classList.remove("effect1");
-        button.classList.add("effect2");
-    }, 2000);
-});
