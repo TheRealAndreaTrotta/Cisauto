10145cfee155a2bf5919396035478b06217c0122
TheRealAndreaTrotta <andreatrotta004@gmail.com>
2024-06-25 15:50:45 +0200

    PARALLAX

--- src/code/js/script.js
+++ src/code/js/script.js
@@ -81,45 +81,16 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-// CIRCLE CURSOR
-const coords = { x: 0, y: 0 };
-const circles = document.querySelectorAll(".circle");
 
-circles.forEach(function(circle) {
-    circle.x = 0;
-    circle.y = 0;
+
+window.addEventListener('scroll', function() {
+    let parallax1 = document.getElementById('parallax-1');
+    let parallax2 = document.getElementById('parallax-2');
+    let parallax3 = document.getElementById('parallax-3');
+    
+    let scrollPosition = window.pageYOffset;
+
+    parallax1.style.backgroundPositionY = scrollPosition * 0.5 + 'px';
+    parallax2.style.backgroundPositionY = scrollPosition * 0.5 + 'px';
+    parallax3.style.backgroundPositionY = scrollPosition * 0.5 + 'px';
 });
-
-window.addEventListener("mousemove", function(e) {
-    coords.x = e.clientX;
-    coords.y = e.clientY;
-});
-
-function animateCircles() {
-    let x = coords.x;
-    let y = coords.y;
-
-    circles.forEach(function(circle, index) {
-        circle.style.left = x - 12 + "px";
-        circle.style.top = y - 12 + "px";
-        circle.style.transform = `scale(${(circles.length - index) / circles.length})`;
-
-        circle.x = x;
-        circle.y = y;
-
-        const nextCircle = circles[(index + 1) % circles.length];
-        x += (nextCircle.x - x) * 0.3;
-        y += (nextCircle.y - y) * 0.3;
-    });
-
-    requestAnimationFrame(animateCircles);
-}
-
-animateCircles();
-
-const cursor = document.getElementById('cursor');
-
-document.addEventListener('mousemove', (e) => {
-    cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-});
-
